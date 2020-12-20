60a9ba2da3bac4ee2d8ef94f9c823821020c8a99
Jameido <rossiluca.dev@gmail.com>
2020-12-20 20:12:34 +0100

    List Repository - Changed repository to get page as input and return domain entity

--- app/src/main/java/dev/jameido/pokedex/data/repository/PkmnRepository.kt
+++ app/src/main/java/dev/jameido/pokedex/data/repository/PkmnRepository.kt
@@ -1,10 +1,10 @@
 package dev.jameido.pokedex.data.repository
 
-import dev.jameido.pokedex.data.models.ResPkmnList
+import dev.jameido.pokedex.domain.entity.PkmnListEntity
 
 /**
  * Created by Jameido on 17/12/2020.
  */
 interface PkmnRepository {
-    suspend fun list(limit: Int, offset: Int) : ResPkmnList
+    suspend fun pkmnList(page: Int) : PkmnListEntity
 }
--- app/src/main/java/dev/jameido/pokedex/framework/PkmnRepositoryImpl.kt
+++ app/src/main/java/dev/jameido/pokedex/framework/PkmnRepositoryImpl.kt
@@ -1,14 +1,22 @@
 package dev.jameido.pokedex.framework
 
 import dev.jameido.pokedex.data.datasource.PkmnDataSource
-import dev.jameido.pokedex.data.models.ResPkmnList
 import dev.jameido.pokedex.data.repository.PkmnRepository
+import dev.jameido.pokedex.domain.entity.PkmnEntity
+import dev.jameido.pokedex.domain.entity.PkmnListEntity
 
 /**
  * Created by Jameido on 17/12/2020.
  */
 class PkmnRepositoryImpl(private val dataSource: PkmnDataSource) : PkmnRepository {
-    override suspend fun list(limit: Int, offset: Int): ResPkmnList {
-        return dataSource.list(limit, offset)
+
+    override suspend fun pkmnList(page: Int): PkmnListEntity {
+        val limit = page * PAGE_SIZE
+        val response = dataSource.list(limit, limit + PAGE_SIZE)
+        return PkmnListEntity(response.next?.let { page + 1 }, response.results.map { PkmnEntity(it.name, it.url) }.toTypedArray())
+    }
+
+    companion object {
+        const val PAGE_SIZE = 5
     }
 }
