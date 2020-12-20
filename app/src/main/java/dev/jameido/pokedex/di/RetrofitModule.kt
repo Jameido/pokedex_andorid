8d26190d65ede5d388d01065551154f272bd816c
Jameido <rossiluca.dev@gmail.com>
2020-12-20 18:54:07 +0100

    List API - Retrofit Koin module

---
+++ app/src/main/java/dev/jameido/pokedex/di/RetrofitModule.kt
@@ -0,0 +1,45 @@
+package dev.jameido.pokedex.di
+
+import com.squareup.moshi.Moshi
+import dev.jameido.pokedex.BuildConfig
+import dev.jameido.pokedex.framework.RetrofitPkmnDataSource
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+/**
+ * Created by Jameido on 18/12/2020.
+ */
+val retrofitModule = module {
+    single { buildOkHttpClient() }
+    factory { buildMoshi() }
+    single { createApiImpl(RetrofitPkmnDataSource::class.java, get(), "https://pokeapi.co/api/v2/") }
+}
+
+private fun buildMoshi(): Moshi {
+    return Moshi.Builder()
+            .build()
+}
+
+private fun buildOkHttpClient(): OkHttpClient {
+    return OkHttpClient.Builder().apply {
+        if (BuildConfig.DEBUG) {
+            val baseLogger = HttpLoggingInterceptor()
+            baseLogger.level = HttpLoggingInterceptor.Level.BODY
+            addInterceptor(baseLogger)
+        }
+    }.build()
+
+    //TODO: Security - SLL Certificate pinning
+}
+
+private fun <S> createApiImpl(apiClass: Class<S>, client: OkHttpClient, baseUrl: String): S {
+    val vRetrofit = Retrofit.Builder()
+            .client(client)
+            .baseUrl(baseUrl)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+    return vRetrofit.create(apiClass)
+}
