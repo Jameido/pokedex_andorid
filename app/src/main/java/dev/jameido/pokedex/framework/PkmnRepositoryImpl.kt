0189ab0ed34517f34b9f3445e3623bc4cab364fc
Jameido <rossiluca.dev@gmail.com>
2020-12-22 09:20:17 +0100

    List Repository - Remote response cache

--- app/src/main/java/dev/jameido/pokedex/framework/PkmnRepositoryImpl.kt
+++ app/src/main/java/dev/jameido/pokedex/framework/PkmnRepositoryImpl.kt
@@ -10,9 +10,41 @@ import dev.jameido.pokedex.domain.entity.PkmnListEntity
  */
 class PkmnRepositoryImpl(private val dataSource: PkmnDataSource) : PkmnRepository {
 
+    private var cache = hashMapOf<Int, PkmnListEntity>()
+    private var cachePageSize = 0
+
     override suspend fun pkmnList(page: Int, pageSize: Int): PkmnListEntity {
+        if (pageSize != cachePageSize) {
+            clearCache()
+            cachePageSize = pageSize
+            readFromService(page, pageSize)
+        }
+        return readFromCache(page) ?: readFromService(page, pageSize)
+
+    }
+
+    private suspend fun readFromService(page: Int, pageSize: Int): PkmnListEntity {
         val offset = page * pageSize
         val response = dataSource.list(offset + pageSize, offset)
-        return PkmnListEntity(response.next?.let { page + 1 }, response.previous?.let { page - 1 }, response.results.map { PkmnEntity(it.name, it.url) })
+        val list = PkmnListEntity(response.next?.let { page + 1 }, response.previous?.let { page - 1 }, response.results.map { PkmnEntity(it.name, it.url) })
+
+        addToCache(page, list)
+
+        return list
+    }
+
+    private fun readFromCache(page: Int): PkmnListEntity? {
+        if (cache.containsKey(page)) {
+            return cache[page]
+        }
+        return null
+    }
+
+    private fun addToCache(page: Int, list: PkmnListEntity) {
+        cache[page] = list
+    }
+
+    private fun clearCache() {
+        cache.clear()
     }
 }
