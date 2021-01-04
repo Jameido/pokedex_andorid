9e918d4b5913f131f362c18b611c41110e83a50f
Jameido <rossiluca.dev@gmail.com>
2021-01-04 15:53:18 +0100

    Species API - Datasource and retrofit interface

--- app/src/main/java/dev/jameido/pokedex/data/datasource/PkmnDataSource.kt
+++ app/src/main/java/dev/jameido/pokedex/data/datasource/PkmnDataSource.kt
@@ -2,6 +2,7 @@ package dev.jameido.pokedex.data.datasource
 
 import dev.jameido.pokedex.data.models.ResPkmnDetail
 import dev.jameido.pokedex.data.models.ResPkmnList
+import dev.jameido.pokedex.data.models.ResPkmnSpecies
 
 /**
  * Created by Jameido on 19/12/2020.
@@ -10,4 +11,6 @@ interface PkmnDataSource {
     suspend fun list(limit: Int, offset: Int) : ResPkmnList
 
     suspend fun detail(name: String) : ResPkmnDetail
+
+    suspend fun species(name: String) : ResPkmnSpecies
 }
--- app/src/main/java/dev/jameido/pokedex/framework/RetrofitPkmnDataSource.kt
+++ app/src/main/java/dev/jameido/pokedex/framework/RetrofitPkmnDataSource.kt
@@ -3,6 +3,7 @@ package dev.jameido.pokedex.framework
 import dev.jameido.pokedex.data.datasource.PkmnDataSource
 import dev.jameido.pokedex.data.models.ResPkmnDetail
 import dev.jameido.pokedex.data.models.ResPkmnList
+import dev.jameido.pokedex.data.models.ResPkmnSpecies
 import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.Path
@@ -21,10 +22,15 @@ interface RetrofitPkmnDataSource : PkmnDataSource {
     @Headers("Content-type: application/json")
     override suspend fun detail(@Path(PATH_NAME) name: String) : ResPkmnDetail
 
+    @GET(SPECIES_ENDPOINT)
+    @Headers("Content-type: application/json")
+    override suspend fun species(@Path(PATH_NAME) name: String) : ResPkmnSpecies
+
     companion object {
         const val LIST_ENDPOINT = "pokemon-species"
         const val PATH_NAME = "name"
         const val DETAIL_ENDPOINT = "pokemon/{$PATH_NAME}"
+        const val SPECIES_ENDPOINT = "pokemon-species/{$PATH_NAME}"
         const val QRY_LIMIT = "limit"
         const val QRY_OFFSET = "offset"
     }
