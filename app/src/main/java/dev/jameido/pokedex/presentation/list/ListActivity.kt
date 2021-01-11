6728685273cd914f9c6fc37d06335118211c7138
Jameido <rossiluca.dev@gmail.com>
2021-01-11 09:16:41 +0100

    Pull to refresh - Accessibility
    Added menu overflow option to trigger refresh without swipe gesture

--- app/src/main/java/dev/jameido/pokedex/presentation/list/ListActivity.kt
+++ app/src/main/java/dev/jameido/pokedex/presentation/list/ListActivity.kt
@@ -1,6 +1,8 @@
 package dev.jameido.pokedex.presentation.list
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
@@ -16,6 +18,7 @@ import kotlinx.android.synthetic.main.activity_list.*
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
+
 
 class ListActivity : AppCompatActivity() {
 
@@ -76,6 +79,21 @@ class ListActivity : AppCompatActivity() {
             }
 
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_list, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_refresh -> {
+                viewModel.refreshContent()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun configRecyclerView() {
